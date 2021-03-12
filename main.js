b227db50eab6d8ce7f692f3b74c8857544b12554
Clementine1703 <62622296+Clementine1703@users.noreply.github.com>
2021-03-12 13:29:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
-// кажется тут должен быть код...
+const text = document.querySelector('#textarea');
+const button = document.querySelector('.news__comment_add button');
+const display = document.querySelector('.comments');
+let comments = JSON.parse(localStorage.getItem('comments'));
 
-// Подсказка №1: Содержимое тега textarea хранится в его свойстве value
 
-// Подсказка №2: Не забывайте, что LocalStorage и SessionStorage могут хранить только строки в виде пар ключ/значение
+if (comments != null) {
+	for (let i = 0; i < comments.length; i++) {
+		let el = document.createElement('p');
+		let comText = comments[i];
+		el.append(comText);
+		display.append(el);
+	}
+}
+else {
+	comments = [];
+}
+
+checkArea();
+
+text.addEventListener('change', checkArea);
+
+button.addEventListener('click', () => {
+	let el = document.createElement('p');
+	let comText = document.createTextNode(text.value);
+	el.appendChild(comText);
+	display.append(el);
+	comments.push(text.value);
+	localStorage.setItem('comments', JSON.stringify(comments));
+	text.value = '';
+})
+
+function checkArea() {
+	if (text.value.split(' ').join('') == '') {
+		text.classList.add('error');
+		button.disabled = true;
+	}
+
+	else {
+		text.classList.remove('error');
+		button.disabled = false;
+	}
+}
